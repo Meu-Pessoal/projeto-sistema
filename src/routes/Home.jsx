2c61c70b0e24bcf4a477f0b2f3de9e3bbaf3494b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 09:53:19 -0300

    arrumando coisas

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
    <section className="bg-gray-900 text-white min-h-screen flex flex-col md:flex-row items-center justify-center p-8 ">
     <div className="flex-1 flex flex-col items-center md:items-start text-center md:text-left mb-8 mr-16">
-      <p className="text-lg text-gray-400">Carros Animados</p>
-      <h4 className="text-4xl md:text-6xl font-bold sm:text-5xl">Um Carro de<br/> Velocidade</h4>
+      <p className="text-lg text-gray-400">Katchau</p>
+      <h4 className="uppercase text-4xl md:text-6xl font-bold sm:text-5xl">Eu sou a<br/> Velocidade</h4>
       <button className="mt-8 px-6 py-3 bg-blue-600 text-white font-bold rounded-full hover:bg-blue-800  transition-colors duration-300">
         Participar
       </button>
