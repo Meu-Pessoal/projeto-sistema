722daff6cb72292be0000019c865983f6cb4a1fc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 09:05:22 -0300

    estilizando o nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,12 @@ import Logo from "../assets/image.png"
 
 const Nav = () => {
   return (
-    <header>
-      <nav>
-        <img src={ Logo } alt="Logo" />
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/cliente">Cliente</Link></li>
+    <header className="bg-orange-900 text-white p-4 shadow-md">
+      <nav className="container mx-auto flex justify-between items-center">
+        <img className="w-20" src={ Logo } alt="Logo" />
+        <ul className="flex gap-2">
+          <li className="list-none"><Link to="/" className="hover:text-gray-400 transition-colors duration-300 hover:underline">Home</Link></li>
+          <li className="list-none"><Link to="/cliente" className="hover:text-gray-400 transition-colors duration-300 hover:underline">Cliente</Link></li>
         </ul>
       </nav>
     </header>
